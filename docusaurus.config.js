912dd436b12589b853e30bb5cd7192ea481ccb3f
Chenxuanwei <3238875613@qq.com>
2023-06-29 23:49:05 +0800

    change Copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,7 +126,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Chenxuanwei, 玄门 Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
